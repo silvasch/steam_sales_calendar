6d57edf4ab6488272db9e814820dcae325b854f0
Silvan Schmidt <mail@silvanschmidt.ch>
2024-10-03 10:56:13 +0200

    feat: generate calendar to out/

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ mod sales;
 pub fn run() {
     let sales = sales::get_sales();
     let ical = make_calendar(&sales);
-    std::fs::write("/tmp/steam_sales.ics", ical).unwrap();
+    std::fs::create_dir("out").unwrap();
+    std::fs::write("out/steam_sales.ics", ical).unwrap();
 }
 
 pub fn make_calendar(sales: &[Sale]) -> String {
